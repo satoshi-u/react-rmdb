41532fbee119408f09f304ee6d9e905b05902f13
Sarthak Joshi <sarthakjoshi@Sarthaks-MacBook-Air.local>
2021-12-05 12:13:31 +0530

    Added login and rate functionality- useContext hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from "react";
 // Styles
-import { GlobalStyle } from './GlobalStyle'
+import { GlobalStyle } from "./GlobalStyle";
 // Components
-import Header from './components/Header'
-import Home from './components/Home'
-import Movie from './components/Movie'
-import NotFound from './components/NotFound'
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Movie from "./components/Movie";
+import NotFound from "./components/NotFound";
+import Login from "./components/Login";
+// Context
+import UserProvider from "./context";
 // Routing
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => (
   // <div className="App">
@@ -17,14 +20,17 @@ const App = () => (
   // </div>
 
   <Router>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/:movieId" element={<Movie />} />
-      <Route path="/*" element={<NotFound />} />
-    </Routes>
-    <GlobalStyle />
+    <UserProvider>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/:movieId" element={<Movie />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+      <GlobalStyle />
+    </UserProvider>
   </Router>
-)
+);
 
-export default App
+export default App;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,22 +1,36 @@
-import React from 'react'
+import React, { useContext } from "react";
 // Images
-import RMDBLogo from '../../images/react-movie-logo.svg'
-import TMDBLogo from '../../images/tmdb_logo.svg'
+import RMDBLogo from "../../images/react-movie-logo.svg";
+import TMDBLogo from "../../images/tmdb_logo.svg";
 // Styles
-import { Wrapper, Content, LogoImg, TMDBLogoImg } from './Header.styles'
+import { Wrapper, Content, LogoImg, TMDBLogoImg } from "./Header.styles";
 // Routing
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
+// Context
+import { Context } from "../../context";
 
-const Header = () => (
-  <Wrapper>
-    <Content>
-      <Link to="/">
-        <LogoImg src={RMDBLogo} alt="rmdb-logo" />
-        {/* {console.log('Should go to home with 1 page only')} */}
-      </Link>
-      <TMDBLogoImg src={TMDBLogo} alt="tmdb-logo" />
-    </Content>
-  </Wrapper>
-)
+const Header = () => {
+  const [user] = useContext(Context);
+  console.log(user);
 
-export default Header
+  return (
+    <Wrapper>
+      <Content>
+        <Link to="/">
+          <LogoImg src={RMDBLogo} alt="rmdb-logo" />
+          {/* {console.log('Should go to home with 1 page only')} */}
+        </Link>
+        {user ? (
+          <span>Logged in as: {user.username}</span>
+        ) : (
+          <Link to="/login">
+            <span>Log in</span>
+          </Link>
+        )}
+        <TMDBLogoImg src={TMDBLogo} alt="tmdb-logo" />
+      </Content>
+    </Wrapper>
+  );
+};
+
+export default Header;
